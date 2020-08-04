828b2b57b7071e272d9114d6f42dd51301fe4796
alcute <alonsoculebras8@gmail.com>
2020-08-04 02:09:53 +0200

    method shortest path completed

--- degrees.py
+++ degrees.py
@@ -92,8 +92,42 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # Set the first node (source)
+    start = Node(state=source, parent=None, action=None)
+    # Initialize frontier
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # explored set is empty
+    explored = set()
+
+    while True:
+
+        # If frontier is empty, then no path
+        if frontier.empty():
+            return None
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+
+        # Mark node as explored
+        explored.add(node.state)
+
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(node.state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+
+                # If child is the goal, then we have a solution
+                if child.state == target:
+                    path = []
+                    while child.parent is not None:
+                        path.insert(0, (child.action, child.state))
+                        child = child.parent
+
+                    return path
+                else:
+                    frontier.add(child)
 
 
 def person_id_for_name(name):
